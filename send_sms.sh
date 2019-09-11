f2d95f60aa787bb7839e26badc23e7da776c86fa
Oscar Calvo <oscar@calvonet.com>
2019-09-10 21:30:12 -0700

    Add SMS notifications

---
+++ send_sms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DEST=$1
+MD="/dev/ttyUSB2"
+
+if [ -z "$2" ]
+then
+    read message
+else
+    message="$2"
+fi
+
+echo "Sending $message to $DEST"
+
+stty -F $MD 9600 min 100 time 2 -hupcl brkint ignpar -opost -onlcr -isig -icanon -echo
+
+chat TIMEOUT 10 "" "AT+CMGF=1" "OK" > $MD < $MD
+chat TIMEOUT 10 "" "AT+CMGS=\"$DEST\"" "OK" > $MD < $MD
+chat TIMEOUT 10 "" "$message^Z" "OK" > $MD < $MD
